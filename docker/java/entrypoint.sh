9acf61a669907f799b012fbddcc8579021fd895a
Roman Nikitin <n75rom@gmail.com>
2021-10-30 03:03:49 +0900

    Release new rc-version

--- docker/java/entrypoint.sh
+++ docker/java/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-cd /init-serv/cib-interns-test-task-v.1.0.0-rc0/
+cd /init-serv/cib-interns-test-task-v.1.0.0-rc1/
 tree
 mvn package
 tree
 echo "pwd: $(pwd)"
-java -jar '/init-serv/cib-interns-test-task-v.1.0.0-rc0/target/api-test-task-0.0.1-SNAPSHOT.jar'
+java -jar '/init-serv/cib-interns-test-task-v.1.0.0-rc1/target/api-test-task-0.0.1-SNAPSHOT.jar'
